b26b9f5503a517c6021efb7761521b464631b19e
thornillos <jl031662@gmail.com>
2022-09-09 11:56:49 -0500

    09/09/2022

---
+++ 14-pdo/config/database.php
@@ -0,0 +1,54 @@
+<?php
+    try {
+        $conx = new PDO("mysql:host=$host;dbname=$name_db",$user,$passwd);
+        $conx->exec("set names utf8");
+        //echo "Connection Successfully!";
+    }
+    catch(PDOException $e) {
+        echo "Connection Error: " . $e->getMessage();
+    }
+
+    //-- CRUD => Pokemons
+
+    // List All Pokemons
+    function listAllPokemons($conx) {
+        try {
+            $sql = "SELECT p.*, t.name AS nametrainer 
+                    FROM pokemons AS p, trainers AS t
+                    WHERE p.trainer_id = t.id";
+            $stm = $conx->prepare($sql);
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    // Show Pokemon
+    function showPokemon($conx, $id) {
+        try {
+            $sql = "SELECT p.*, t.name AS nametrainer 
+                    FROM pokemons AS p, trainers AS t
+                    WHERE p.id = :id AND p.trainer_id = t.id";
+            $stm = $conx->prepare($sql);
+            $stm->bindparam(":id", $id);
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    // Trainers
+
+    // List All Trainers
+    function listAllTrainers($conx) {
+        try {
+            $sql = "SELECT * FROM trainers";
+            $stm = $conx->prepare($sql);
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
